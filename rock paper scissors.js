3f27d93c00bf9e9391275c7ecf9e4229a7f6e652
sataratimoori <sataratimoori@gmail.com>
2026-08-29 19:12:05 +0430

    Add DOM element references

---
+++ rock paper scissors.js
@@ -0,0 +1,18 @@
+// Get elements from HTML
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
+let playerChoice = document.getElementById("player-choice");
+let computerChoice = document.getElementById("computer-choice");
+
+let playerChoiceName = document.getElementById("player-choice-name");
+let computerChoiceName = document.getElementById("computer-choice-name");
+
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+
+let result = document.getElementById("result");
+let round = document.getElementById("round");
+
+let reset = document.getElementById("reset");
