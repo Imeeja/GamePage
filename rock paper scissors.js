5709eeb117f517850d3b2d2d46a38bd2d66a5550
sataratimoori <sataratimoori@gmail.com>
2026-08-29 19:32:08 +0430

    Add random computer choice function

--- rock paper scissors.js
+++ rock paper scissors.js
@@ -18,9 +18,18 @@ let round = document.getElementById("round");
 let reset = document.getElementById("reset");
 
 
-
+// Game variables
 let playerPoints = 0;
 let computerPoints = 0;
 let currentRound = 1;
 
 let choices = ["rock", "paper", "scissors"];
+
+
+// Computer choices
+function computerChoose() {
+
+    let randomNumber = Math.floor(Math.random() * choices.length);
+
+    return choices[randomNumber];
+}
